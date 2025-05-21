c9382c382677ab13d6d2df188f788ce04e74be3a
Henrique Pierandrei <henriquepierandrei1@gmail.com>
2025-05-21 15:51:01 -0300

    fix: atualizei o elemento que não estava sendo usado.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import "./Footer.css";
 
 
 function Footer() {
   return (
     <div className='footer-container-principal'>
+      <br /><br />
       Copyright © 2025 portfolio-henrique-pierandrei.vercel.app. Todos os direitos reservados.
     </div>
   )
